029aaf6a82b205c733b03c86db07ebc0eb1c877d
TIMUR <1tkurbanov@gmail.com>
2019-05-24 15:51:17 -0400

    done

---
+++ photogur/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from photogur.models import Picture
+
+def root(request):
+    return HttpResponseRedirect('pictures')
+
+def pictures_page(request):
+    context = {'pictures': Picture.objects.all()}
+    response = render(request, 'pictures.html', context)
+    return HttpResponse(response)
+
+    
