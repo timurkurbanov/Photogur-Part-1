584d2a7ca471893381d3f57172a02c49dd29f83f
TIMUR <1tkurbanov@gmail.com>
2019-05-31 15:43:44 -0400

    85%

--- photogur/views.py
+++ photogur/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, reverse, redirect
 from photogur.models import Picture, Comment
 from photogur.forms import LoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 def root(request):
 	return HttpResponseRedirect('/pictures')
@@ -66,6 +67,22 @@ def login_view(request):
 def logout_view(request):
 	logout(request)
 	return HttpResponseRedirect('/pictures')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect('/pictures')
+    else:
+        form = UserCreationForm()
+    html_response =  render(request, 'signup.html', {'form': form})
+    return HttpResponse(html_response)
+
 
 # https://docs.djangoproject.com/en/2.2/topics/http/shortcuts/#example
 # def picture_show(request, id):
